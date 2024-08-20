5eea0a394b94102584d29632c2fae3c8dd2df04a
Vitor figueiredo <vitor.figueiredo@neogov.com.br>
2024-08-20 10:43:23 -0300

    Criando pasta separada para eventos

--- src/index.mjs
+++ src/index.mjs
@@ -6,11 +6,11 @@ import 'dotenv/config';
 const client = new Client({ intents: [GatewayIntentBits.Guilds],  rest: { timeout: 60000 } });
 client.commands = new Collection();
 
-const mainFolderPath = 'src/commands/';
-const commandFolders = fs.readdirSync(mainFolderPath);
+const commandsDir = 'src/commands/';
+const commandFolders = fs.readdirSync(commandsDir);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(mainFolderPath, folder);
+	const commandsPath = path.join(commandsDir, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.mjs'));
 	
   for (const file of commandFiles) {
@@ -28,30 +28,22 @@ for (const folder of commandFolders) {
 	
 }
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+ const eventsDir = 'src/events/'
+ const eventsFiles = fs.readdirSync(eventsDir).filter(file => file.endsWith('.mjs'))
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+ for(const file of eventsFiles) {
+	const filePath = path.join(eventsDir, file)
+	const fileUrl = new URL(`file://${path.resolve(filePath)}`)
+	const event = await import(fileUrl.href)
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+	const eventDefault = event.default
+	if (eventDefault.once) {
+		client.once(eventDefault.name, (...args) => eventDefault.execute(...args));
+	} else {
+		client.on(eventDefault.name, (...args) => eventDefault.execute(...args));
 	}
+ }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+
 
 client.login(process.env.TOKEN);
